7b56eefe32999508130d3ad7ab979d5c2f7fbdf0
Addinul Kintangi <addins3009@gmail.com>
2017-07-22 09:20:28 +0700

    add authoritiesConstants

--- src/main/java/io/github/semhas/security/AuthoritiesConstants.java
+++ src/main/java/io/github/semhas/security/AuthoritiesConstants.java
@@ -11,6 +11,14 @@ public final class AuthoritiesConstants {
 
     public static final String ANONYMOUS = "ROLE_ANONYMOUS";
 
+    public static final String DOSEN = "ROLE_DOSEN";
+
+    public static final String MAHASISWA = "ROLE_MAHASISWA";
+
+    public static final String PRODI = "ROLE_PRODI";
+
+    public static final String ADMIN_RUANG = "ROLE_ADMIN_RUANG";
+
     private AuthoritiesConstants() {
     }
 }
