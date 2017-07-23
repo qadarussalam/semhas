b01673c416437c7814d601e1bb878808b6795179
Addinul Kintangi <addins3009@gmail.com>
2017-07-23 11:40:00 +0700

    allow publid access to POST /api/register/mahasiswa

--- src/main/java/io/github/semhas/config/SecurityConfiguration.java
+++ src/main/java/io/github/semhas/config/SecurityConfiguration.java
@@ -101,6 +101,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         .and()
             .authorizeRequests()
             .antMatchers("/api/register").permitAll()
+            .antMatchers("/api/register/mahasiswa").permitAll()
             .antMatchers("/api/activate").permitAll()
             .antMatchers("/api/authenticate").permitAll()
             .antMatchers("/api/account/reset_password/init").permitAll()
