5e5dbf52243300dfa869c8395f9e3be6a67dbb4f
Addinul Kintangi <addins3009@gmail.com>
2017-07-23 09:46:22 +0700

    allow public access to GET /api/jurusans/**

--- src/main/java/io/github/semhas/config/SecurityConfiguration.java
+++ src/main/java/io/github/semhas/config/SecurityConfiguration.java
@@ -79,7 +79,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/content/**")
             .antMatchers("/swagger-ui/index.html")
             .antMatchers("/test/**")
-            .antMatchers("/h2-console/**");
+            .antMatchers("/h2-console/**")
+            .antMatchers(HttpMethod.GET, "/api/jurusans/**");
     }
 
     @Override
